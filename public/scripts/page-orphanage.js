c585a825b2ee932a8509ba7bae3aa03beedfaaba
DiegoGamesFight <35296262+diogoditorr@users.noreply.github.com>
2020-10-14 22:55:27 -0300

    Workshop 02

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,41 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClick: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+}
+
+const map = L.map('mapid', options).setView([-27.222633, -49.6455874], 15);
+
+// create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+// create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+});
+
+// create and add marker
+L.marker([-27.222633, -49.6455874], { icon: icon })
+    .addTo(map)
+
+/* image gallery */
+function selectImage(event) {
+    const button = event.currentTarget
+    
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach((button) => {
+        button.classList.remove("active")
+    })
+
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+
+    imageContainer.src = image.src
+
+    button.classList.add('active')
+}
