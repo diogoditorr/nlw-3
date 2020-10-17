0ccea98ac6cbea44bf89bd58b71d59923cc180f7
DiegoGamesFight <35296262+diogoditorr@users.noreply.github.com>
2020-10-16 22:51:00 -0300

    Workshop 4

---
+++ src/pages.js
@@ -0,0 +1,24 @@
+const orphanages = require('./database/fakedata.js')
+
+module.exports = {
+
+    index(request, response) {
+        const city = request.query.city
+
+        return response.render('index')
+    },
+
+    orphanage(request, response) {
+        return response.render('orphanage')
+    },
+
+    orphanages(request, response) {
+        return response.render('orphanages', { orphanages })
+    },
+
+    createOrphanage(request, response) {
+        return response.render('create-orphanage')
+
+    }
+
+}
